8d00f8bc937579224a76ad501bf66d2f15cfea74
reilag <reilag69@gmail.com>
2016-09-20 17:04:20 +0300

    Create AntiCaptcha.php

---
+++ src/AntiCaptcha.php
@@ -0,0 +1,16 @@
+<? namespace AntiCaptchaV2;
+
+/**
+ * Class AntiCaptcha
+ * @package AntiCaptchaV2
+ */
+class AntiCaptcha
+{
+    /**
+     * AntiCaptcha constructor.
+     */
+    public function __construct($service = null, array $options = [])
+    {
+        
+    }
+}
